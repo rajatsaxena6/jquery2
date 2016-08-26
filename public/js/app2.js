f547e5fa9fb58bcf0ed38c4f021b8bea6a775cb7
rajatsaxena6 <rajat.saxena6@wipro.com>
2016-08-26 18:48:48 +0530

    jq1

--- public/js/app2.js
+++ public/js/app2.js
@@ -1,53 +1,144 @@
 var app2 = angular.module('app2', []);
 
-app2.controller('formController', function($scope, $http) {
+app2.controller('formController', function($scope, dataservice) {
+	$scope.page = 3;
+	$scope.hidden = true;
+	$scope.options = ["Please Choose","Money Market Account","Credit Card Account","Auto Loan Account","Personal Loan Account",
+	"Investment Account","Checking Account","Home Loan Account","Savings Account"];
 
-    $scope.searchclick = function() {
+	$scope.searchclick = function() {
 
-        alert($scope.searchvalue);
+	dataservice.searchone($scope.searchvalue).success(function(response){
 
-        $http.get('http://localhost:8080/people?name=' + $scope.searchvalue).success(function(response) {
+    		$scope.peopledata = response;
+    	
+    });
 
-            $scope.peopledata = response;
-        });
-    };
+	};
+
+	$scope.next = function() {
+
+		
+		dataservice.nextpage($scope.page).success(function(response){
+
+			$scope.peopledata = response;
+			$scope.page = $scope.page + 1;
+		})
+		
+	};
+
+	$scope.prev = function() {
+
+		
+		dataservice.nextpage($scope.page).success(function(response){
+
+			$scope.peopledata = response;
+			$scope.page = $scope.page - 1;
+		})
+		
+	};
 
     $scope.viewclick = function() {
+    	$scope.hidden = false;
 
-        $http.get('http://localhost:8080/people/?&_limit=20').success(function(response) {
+        dataservice.searchall().success(function(response) {
 
             $scope.peopledata = response;
-
+            
         });
     };
 
     $scope.deleteclick = function(id) {
-        console.log("Inside delete");
+        
 
-        console.log(id);
         var userId = $scope.peopledata[id].id;
-        console.log(userId);
-        $http.delete('http://localhost:8080/people/' + userId).success(function(response) {
+        
+        
+        dataservice.delete(userId).success(function(response) {
 
             $scope.peopledata.splice(id, 1);
 
         });
     };
 
-    $scope.submitclick = function(name, email, phonenumber, accounttype) {
-	alert(name);
-        var ojb = {
-            name: name,
-            email: email,
-            phonenumber: phonenumber,
-            accountname: accounttype
-        };
+    $scope.submitclick = function(ojb) {
+    	
+        
 
-        $http.post('http://localhost:8080/people', ojb).success(function(response) {
-
+        dataservice.add(ojb).success(function(response) {
+        	console.log(response);
             $scope.peopledata = [response];
         });
     };
 
+    $scope.showEditRow = function(r) {
+    	if($scope.active!=r){
+    		$scope.active = r;
+    	}
+    	else{
+    		$scope.active = null;
+    	}
+    }
 
+    $scope.update = function(oj,id) {
+
+    	dataservice.update(oj,id).success(function(response){
+
+    		console.log(response);
+    		$scope.peopledata = [response];
+    	})
+    }
+
+    $scope.master = {};
+   
 });
+
+
+
+
+
+//dataservice factory..
+
+app2.factory('dataservice',['$http',function($http){
+
+	var obj = {};
+
+	obj.searchone = function(text){
+
+ 		return $http.get('http://localhost:8080/people?name='+text); 
+	}
+
+	obj.searchall = function(){
+
+		return $http.get('http://localhost:8080/people/?&_limit=20');
+	}
+
+	obj.delete = function(id){
+
+		return $http.delete('http://localhost:8080/people/'+id);
+	}
+
+	obj.add = function(object){
+		
+		return $http.post('http://localhost:8080/people', object);
+	}
+
+	obj.update = function(myob,id){
+		return $http.put('http://localhost:8080/people/'+id,myob);
+	}
+
+	obj.nextpage = function(start){
+		return $http.get('http://localhost:8080/people/?_page='+start);
+	}
+
+	
+	return obj;
+}]);
+
+
+
+
+
+
+
+	
---
+++ public/js/route.js
@@ -0,0 +1,6 @@
+angular.module('app2')
+.config(function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl: 'template.html',
+	})
+});
